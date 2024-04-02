ac19298c1edb6e0797879091a5bc80560ffb7173
Yahya Namir <yahyanamir5@gmail.com>
2024-04-02 15:13:58 +0000

    testName
    name

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
     <header className="header">
       <nav className="nav container">
         <a href="/" className="nav__logo">
-          YAHYA
+          YAHYA NAMIR
         </a>
         <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list grid">
